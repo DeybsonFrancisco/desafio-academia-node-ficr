99e24209056e9d36da95dc8b51e41bc1cda14d91
DEYBSON FRANCISCO DA SILVA <FR041451@catolica.ubec>
2020-01-30 21:48:38 -0300

    ajustes

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,11 @@ const { Router } = require("express");
 const NotFound = require("./middlewares/NotFound");
 const CurriculoController = require("./controllers/CurriculoController");
 const ErrorHandler = require("./helpers/ErrorHelper");
-const facebook = require("./services/Facebook");
+const path = require("path");
 
 const app = Router();
 
 app.get("/curriculo/:user", CurriculoController.getCurriculo);
-app.get("/face", facebook);
 app.use(NotFound);
 app.use((err, req, res, next) => {
     if (!err.statusCode) err.statusCode = 500;
--- src/services/Facebook.js
+++ src/services/Facebook.js
@@ -5,7 +5,7 @@ const facebookUserInfo = async () => {
         const userInfo = await Axios.get(
             `https://graph.facebook.com/v5.0/me?access_token=${process.env.USER_TOKEN_FACEBOOK}&debug=all&fields=id%2Cname%2Cbirthday%2Ceducation%2Cemail%2Clocation&format=json&method=get&pretty=0&suppress_http_code=1&transport=cors`
         );
-        console.log(userInfo.data);
+
         return userInfo.data;
     } catch (err) {
         return err;
