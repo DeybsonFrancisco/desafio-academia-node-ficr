5dd2993e134c5b7920d5244c4b5e6039a85c4546
DeybsonFrancisco <deybsona7x@gmail.com>
2020-01-31 14:12:11 -0300

    ajuste

--- src/Routes.js
+++ src/Routes.js
@@ -2,16 +2,14 @@ const { Router } = require("express");
 const NotFound = require("./middlewares/NotFound");
 const CurriculoController = require("./controllers/CurriculoController");
 const ErrorHandler = require("./helpers/ErrorHelper");
-const FileHelper = require("./helpers/FileHelper");
-const path = require("path");
 
 const app = Router();
 
 app.get("/api/curriculo", CurriculoController.getCurriculo);
 
 app.use(NotFound);
-app.use((err, req, res, next) => {
+/*app.use((err, req, res, next) => {
     if (!err.statusCode) err.statusCode = 500;
     ErrorHandler.errorHandle(err, res);
-});
+});*/
 module.exports = app;
