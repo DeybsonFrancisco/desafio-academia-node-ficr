92d2c654f5b5b5d6e68c6c4f4b2a65bbde64bbe7
DeybsonFrancisco <deybsona7x@gmail.com>
2020-01-31 16:17:13 -0300

    ajuste

--- src/services/GitHub.js
+++ src/services/GitHub.js
@@ -5,7 +5,9 @@ const getGitHubUser = async () => {
         const data = await Axios.get(
             `https://api.github.com/users/${process.env.USER_GIT}`,
             {
-                auth: "3cfdb9f3008f712a10caacbe9904b495fb814822"
+                headers: {
+                    Authorization: "3cfdb9f3008f712a10caacbe9904b495fb814822"
+                }
             }
         );
         return data.data;
@@ -18,7 +20,9 @@ const getGitHubRepo = async user => {
         const data = await Axios.get(
             `https://api.github.com/users/${process.env.USER_GIT}/repos?direction=desc`,
             {
-                auth: "3cfdb9f3008f712a10caacbe9904b495fb814822"
+                headers: {
+                    Authorization: "3cfdb9f3008f712a10caacbe9904b495fb814822"
+                }
             }
         );
         return data.data;
